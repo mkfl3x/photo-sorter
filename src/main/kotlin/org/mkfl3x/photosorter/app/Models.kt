d5252b072b47129a6fc784fa313b6e631613adf8
Mikhail Kuznetsov <mikhail.kuznetsov@jetbrains.com>
2023-06-24 03:13:34 +0300

    file class moved to models

--- src/main/kotlin/org/mkfl3x/photosorter/app/Models.kt
+++ src/main/kotlin/org/mkfl3x/photosorter/app/Models.kt
@@ -1,5 +1,11 @@
 package org.mkfl3x.photosorter.app
 
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.nio.file.attribute.BasicFileAttributes
+import java.text.SimpleDateFormat
+
 enum class SortMode(val text: String) {
     COPY("Copy"),
     MOVE("Move"),
@@ -12,3 +18,24 @@ enum class FolderType {
 }
 
 class DirectoryException(message: String?) : Exception(message)
+
+class File(val filepath: Path, private val mode: SortMode) {
+
+    private val filenamePattern =
+        if (System.getProperty("os.name").startsWith("Mac")) "yyyy-MMM-dd_HH-mm-ss" else "yyyy-MMM-dd_HH:mm:ss"
+
+    fun getDestinationFilepath(sourceFolder: String, destinationFolder: String): Path = Paths.get(
+        filepath.toString().replace(filepath.fileName.toString(), formatFilename()).let {
+            if (mode != SortMode.REPLACE) it.replace(sourceFolder, destinationFolder) else it
+        }
+    )
+
+    private fun formatFilename() =
+        SimpleDateFormat(filenamePattern).format(getLastModifiedTime().toMillis()) + ".${getExtension() ?: "?"}"
+
+    private fun getLastModifiedTime() =
+        Files.readAttributes(filepath, BasicFileAttributes::class.java).lastModifiedTime()
+
+    private fun getExtension() =
+        Files.probeContentType(filepath)?.substringAfterLast("/")
+}
--- src/main/kotlin/org/mkfl3x/photosorter/app/PhotoSorter.kt
+++ src/main/kotlin/org/mkfl3x/photosorter/app/PhotoSorter.kt
@@ -4,8 +4,6 @@ import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
-import java.nio.file.attribute.BasicFileAttributes
-import java.text.SimpleDateFormat
 import javax.swing.JOptionPane
 import javax.swing.JTextArea
 import kotlin.io.path.isDirectory
@@ -80,25 +78,4 @@ class PhotoSorter {
         null,
         null
     )
-}
-
-private class File(val filepath: Path, private val mode: SortMode) {
-
-    private val filenamePattern =
-        if (System.getProperty("os.name").startsWith("Mac")) "yyyy-MMM-dd_HH-mm-ss" else "yyyy-MMM-dd_HH:mm:ss"
-
-    fun getDestinationFilepath(sourceFolder: String, destinationFolder: String): Path = Paths.get(
-        filepath.toString().replace(filepath.fileName.toString(), formatFilename()).let {
-            if (mode != SortMode.REPLACE) it.replace(sourceFolder, destinationFolder) else it
-        }
-    )
-
-    private fun formatFilename() =
-        SimpleDateFormat(filenamePattern).format(getLastModifiedTime().toMillis()) + ".${getExtension() ?: "?"}"
-
-    private fun getLastModifiedTime() =
-        Files.readAttributes(filepath, BasicFileAttributes::class.java).lastModifiedTime()
-
-    private fun getExtension() =
-        Files.probeContentType(filepath)?.substringAfterLast("/")
 }
